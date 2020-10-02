2881ba8c4f9e06d82047286019f57e3abb7d8db7
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-02 05:46:26 -0500

    adding initial files

---
+++ script.js
@@ -0,0 +1,15 @@
+/* README INSTRUCTIONS
+Simple Calendar application
+User can save events for each hour of the day
+Features dynamically updated HTML and CSS powered by jQuery
+Need to use Moment.js library to work with date and time
+
+REQUIREMENTS
+- Current date displays at the top of the calendar
+- Time blocks appear for standard business hours
+- Time blocks are color coded depending on the actual time of day
+    (Past, Present, Future)
+- Can click on time block and add an event
+- When page is refreshed, edited time block should still appear */
+
+console.log("TEST")
