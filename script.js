5f0ba04e41282ce561b344f08cf8a5d689bb97ac
Alex Eesley <alexandra.eesley@gmail.com>
2020-10-02 06:04:31 -0500

    added current date functionality to top of the page

--- script.js
+++ script.js
@@ -12,4 +12,17 @@ REQUIREMENTS
 - Can click on time block and add an event
 - When page is refreshed, edited time block should still appear */
 
-console.log("TEST")
+$(document).ready(function() {
+
+    // Getting current data & time to show up in header area
+    // defining the variable mapped to the id on HTML
+    var currentDay = $("#currentDay");
+    // using the moments.js syntax to define variable with the dates
+    currentDay = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+    // pushing the current date variable to the right location on HTML
+    $("#currentDay").text(currentDay);
+
+
+
+
+});
